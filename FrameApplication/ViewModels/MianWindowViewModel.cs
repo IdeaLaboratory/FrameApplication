2ec0c9e77b64e9b041fd230c80ff712c72d7bc4d
AdiTCG <adithecomputerguy@gmail.com>
2019-03-04 03:27:34 +0530

    MVVM structure v0.01

---
+++ FrameApplication/ViewModels/MianWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameApplication.ViewModels
+{
+    public class MianWindowViewModel : ViewModelBase
+    {
+        public string AppTitle { get; set; } = "FrameApplication";
+    }
+}
---
+++ FrameApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FrameApplication.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Invoked whenever the effective value of any dependency property on this DependencyObject has been updated.
+        /// CallerMemberName obtain the method or property name of the caller to the method
+        /// </summary>
+        /// <param name="PropertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
