442a85942b67aec455a53e8e6bc2a9733a061e5a
Adi Soumitra Mondal <adithecomputerguy@gmail.com>
2020-08-25 09:25:38 +0530

    Created new branch "MVVM with Caliburn". Restructured this project to prepare for Caliburn.Micro

--- FrameApplication/ViewModels/MianWindowViewModel.cs
+++ FrameApplication/ViewModels/MianWindowViewModel.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Diagnostics;
 
 namespace FrameApplication.ViewModels
 {
-    public partial class MianWindowViewModel : ViewModelBase
+    public partial class MianWindowViewModel
     {
         public string AppTitle { get; set; } = "FrameApplication";
 
         public void Execute()
         {
-            MessageBox.Show("hii");
             Trace.WriteLine("Execute command");
         }
     }
