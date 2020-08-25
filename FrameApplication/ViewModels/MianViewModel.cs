bf6e253df2a06f3b2f58ee65662db34f6246851e
Adi Soumitra Mondal <adithecomputerguy@gmail.com>
2020-08-25 12:32:42 +0530

    NotifyOfPropertyChange: If the value changed it was not able to refresh the value on UI yet. NotifyOfPropertyChange(() => Text); will broadcast a notification once it is changed.

--- FrameApplication/ViewModels/MianViewModel.cs
+++ FrameApplication/ViewModels/MianViewModel.cs
@@ -3,14 +3,22 @@ using System.Diagnostics;
 
 namespace FrameApplication.ViewModels
 {
-    public partial class MainViewModel : Screen //: Screen (opening closing events are already available) OR 
+    public partial class MainViewModel : Screen //: Screen (opening closing events are already available) OR
     {
+        private string _text = "Default text";
         public string AppTitle { get; set; } = "FrameApplication";
-
         public string Text
         {
-            get; set;
-        } = "Default text";
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                NotifyOfPropertyChange(() => Text);
+            }
+        }
 
         public void Execute()
         {
